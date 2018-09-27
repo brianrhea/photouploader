642e77638b35b8443c36ecc809334ad010cfe294
Brian Rhea <brianrhea@gmail.com>
2018-09-27 17:22:12 -0600

    db

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
 
 inp.onchange = e => {
+
   Promise.all([...inp.files].map(toThumbnail))
     .then(function(imgs){
       $(imgs).each(function(index, img){
@@ -84,3 +85,37 @@ function collectImageData() {
 $('#upload').on('click', function(){
   collectImageData();
 });
+
+function uploadFile(file) {
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.upload.onprogress = function(evt) {
+      var percentComplete = parseInt(100.0 * evt.loaded / evt.total);
+      // Upload in progress. Do something here with the percent complete.
+  };
+
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+          var fileInfo = JSON.parse(xhr.response);
+          // Upload succeeded. Do something here with the file info.
+      }
+      else {
+          var errorMessage = xhr.response || 'Unable to upload file';
+          // Upload failed. Do something here with the error.
+      }
+  };
+
+  xhr.open('POST', 'https://content.dropboxapi.com/2/files/upload');
+  xhr.setRequestHeader('Authorization', 'Bearer eB1-0FMyyqAAAAAAAAAACTnGrf8Wf_t0-81yiRxs73IhYltggC7U-K61BTDiNol7');
+  xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+  xhr.setRequestHeader('Dropbox-API-Arg', JSON.stringify({
+      path: '/' +  file.name,
+      mode: 'add',
+      autorename: true,
+      mute: false
+  }));
+
+  xhr.send(file);
+
+}
